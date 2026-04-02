196b8fda50dc47a4944fcd35d10742977e7b2c00
ahmdzoo <alamqztb@gmail.com>
2026-04-02 12:53:12 +0700

    Kelola Jadwal

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -37,6 +37,13 @@
                 <span x-show="open" class="ml-4 font-bold text-xs uppercase italic tracking-widest">Kelola Dokter</span>
             </a>
 
+            <a href="{{ route('schedules.index') }}" class="sidebar-link {{ request()->routeIs('schedules.*') ? 'active-link' : '' }}" :class="open ? 'justify-start px-3' : 'justify-center'">
+                <div class="icon-box">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2-2v12a2 2 0 002 2z"/></svg>
+                </div>
+                <span x-show="open" class="ml-4 font-bold text-xs uppercase italic tracking-widest">Kelola Jadwal</span>
+            </a>
+
             <a href="{{ route('articles.index') }}" class="sidebar-link {{ request()->routeIs('articles.*') ? 'active-link' : '' }}" :class="open ? 'justify-start px-3' : 'justify-center'">
                 <div class="icon-box">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1M19 20a2 2 0 002-2V12M19 20a2 2 0 01-2-2m2-2h-4m-4 4V5"/></svg>
